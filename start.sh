7f9c05938da405edb4f943006824f7248674b2b1
Rimuru <93518654+RimuruVN@users.noreply.github.com>
2021-11-10 22:13:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/RimuruVN/VideoPlayerTG -b $BRANCH /VideoPlayerTG
 fi
-cd /VCPlayerBot
+cd /VideoPlayerTG
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
