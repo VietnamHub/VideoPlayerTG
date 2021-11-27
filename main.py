d25955692d3a125e10a51472fed8747b01cd7135
gimsuri69 <94640040+gimsuri69@users.noreply.github.com>
2021-11-27 14:43:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def main():
                 msg=await db.get_config("RESTART")
                 if msg:
                     try:
-                        k=await bot.edit_message_text(msg['chat_id'], msg['msg_id'], text="Succesfully restarted.")
+                        k=await bot.edit_message_text(msg['chat_id'], msg['msg_id'], text="Khởi động lại thành công.")
                         await db.del_config("RESTART")
                     except:
                         pass
