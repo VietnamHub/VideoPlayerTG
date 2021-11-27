11f2586776a080a431e8a2291b6bb27701e72451
gimsuri69 <94640040+gimsuri69@users.noreply.github.com>
2021-11-27 14:43:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
-  echo "Cloning main branch...."
-  git clone https://github.com/RimuruVN/VideoPlayerTG /VideoPlayerTG
+  echo "Nhân bản chi nhánh chính...."
+  git clone https://github.com/gimsuri69/VideoPlayerTG /VideoPlayerTG
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/RimuruVN/VideoPlayerTG -b $BRANCH /VideoPlayerTG
+  git clone https://github.com/gimsuri69/VideoPlayerTG -b $BRANCH /VideoPlayerTG
 fi
 cd /VideoPlayerTG
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Khởi động Bot...."
 python3 main.py
